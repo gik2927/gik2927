8545e2e78d87e9f24ca96d900ffcf0e2845a3515
gik2927 <59925086+gik2927@users.noreply.github.com>
2021-03-23 23:01:01 +0900

    Update 반도박.py

--- 반도박.py
+++ 반도박.py
@@ -1,6 +1,10 @@
 from random import*
 num = (randint(1,100))
+
+print("\t=====반도박=====\n")
+
 if num > 50:
-    print("도박 성공!")
+    print("\t   도박 성공!")
 else:
-    print("도박 실패 ...")
+    print("\t   도박 실패 ...")
+
